608e5e6bb51fa0c40f301251d9193a4ca8372d9d
Kolt Ewing <koltewing@gmail.com>
2021-01-14 01:48:08 -0500

    add my cards screen and sidebar (#20)

--- pages/index.styles.ts
+++ pages/index.styles.ts
@@ -1,6 +1,44 @@
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/core/styles'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.up('lg')]: {
+      display: 'flex',
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('lg')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  sidebarLogo: {
+    maxWidth: '100%',
+    maxHeight: 64,
+  },
+  drawer: {
+    width: 100,
+    [theme.breakpoints.up('lg')]: {
+      width: drawerWidth,
+    },
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    [theme.breakpoints.up('lg')]: {
+      width: drawerWidth,
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: {
+    ...theme.mixins.toolbar,
+    textAlign: 'center',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
   footer: {
     width: '100%',
     position: 'fixed',
