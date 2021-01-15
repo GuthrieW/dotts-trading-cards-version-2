b74e6a1f44644d5a1cb6eb74b1df1f97704e845c
koltclassic <koltewing@gmail.com>
2021-01-15 09:52:34 -0500

    modularize collection view for both my cards and community pages

--- pages/Community/[user].tsx
+++ pages/Community/[user].tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import CollectionView from '../../components/CollectionView/CollectionView';
 
 const CommunityMemberPage = () => {
   const router = useRouter()
   const { user } = router.query;
 
-  return <h1>{user}'s Cards</h1>;
+  return (
+    <CollectionView />
+  );
 }
 
 export default CommunityMemberPage;
