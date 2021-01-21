d48ac85f96f451a65e4e2d624022a3e8eb73126c
GuthrieW <wesleyeguthrie@gmail.com>
2021-01-20 23:53:24 -0600

    Added interfaces for card and dottsAccounts

---
+++ pages/api/database/models/cards.ts
@@ -0,0 +1,10 @@
+export interface cards {
+  playerName: String // player_name
+  playerTeam: String // player_team
+  rarity: String // rarity
+  imageUrl: String // image_url
+  submissionUsername: String // submission_username
+  submissionDate: Date // submission_date
+  approved: Boolean // approved
+  currentRotation: Boolean // current_rotation
+}
