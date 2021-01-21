f03e7bb84574d29246561558b713b62db5da2404
Kolt Ewing <koltewing@gmail.com>
2021-01-20 23:20:04 -0500

    add pages for opening packs (#22)
    * add pages for opening packs
    
    * remove unused code

--- pages/index.styles.ts
+++ pages/index.styles.ts
@@ -37,12 +37,12 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
   },
   footer: {
     width: '100%',
     position: 'fixed',
     bottom: 0,
+    zIndex: 1,
   },
 }))
 
