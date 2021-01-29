c573897d935c7825661e6c23f7102278e31d76fa
GuthrieW <wesleyeguthrie@gmail.com>
2021-01-29 11:45:46 -0600

    Removed comments from schema

--- pages/api/database/models/dottsCards.ts
+++ pages/api/database/models/dottsCards.ts
@@ -1,10 +1,10 @@
 export interface dottsCards {
-  playerName: String // player_name
-  playerTeam: String // player_team
-  rarity: String // rarity
-  imageUrl: String // image_url
-  submissionUsername: String // submission_username
-  submissionDate: Date // submission_date
-  approved: Boolean // approved
-  currentRotation: Boolean // current_rotation
+  playerName: String
+  playerTeam: String
+  rarity: String
+  imageUrl: String
+  submissionUsername: String
+  submissionDate: Date
+  approved: Boolean
+  currentRotation: Boolean
 }
