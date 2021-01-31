13ec5944e283701e9c64e183491681d8925284f0
GuthrieW <wesleyeguthrie@gmail.com>
2021-01-31 13:56:10 -0600

    Put layout inside card

--- layouts/SplashLayout.tsx
+++ layouts/SplashLayout.tsx
@@ -4,9 +4,24 @@
 //   },
 // })
 import React from 'react'
+import { Card, CardContent } from '@material-ui/core'
 
 const SplashLayout = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <Card>
+      <CardContent>
+        <div
+          style={{
+            display: 'inline-block',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {children}
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default SplashLayout
