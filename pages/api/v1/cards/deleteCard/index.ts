3ec2f2bfc7b84c8fbd38bdf6f1de4146e89a99ce
GuthrieW <wesleyeguthrie@gmail.com>
2021-02-07 15:08:29 -0600

    Added closing connections

--- pages/api/v1/cards/deleteCard/index.ts
+++ pages/api/v1/cards/deleteCard/index.ts
@@ -4,12 +4,14 @@ import _ from 'lodash'
 import { ObjectId } from 'mongodb'
 
 const index = async (request: NextApiRequest, response: NextApiResponse) => {
-  const { database } = await connect()
+  const { database, client } = await connect()
   const { cardId } = request.body
 
   const result = await database
     .collection('dotts_cards')
     .findOneAndDelete({ _id: new ObjectId(cardId) })
+
+  client.close()
 
   response.status(200).send(result)
 }
--- pages/api/v1/cards/unapprovedCards/index.ts
+++ pages/api/v1/cards/unapprovedCards/index.ts
@@ -3,12 +3,13 @@ import { connect } from '../../../database/database'
 import _ from 'lodash'
 
 const index = async (request: NextApiRequest, response: NextApiResponse) => {
-  const { database } = await connect()
+  const { database, client } = await connect()
 
   const result = await database
     .collection('dotts_cards')
     .find({ approved: false })
     .toArray()
+  client.close()
 
   response.status(200).send(result)
 }
