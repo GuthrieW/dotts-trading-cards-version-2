eeb2d6160a2ccdc803ce141c9be594cd7199db56
GuthrieW <wesleyeguthrie@gmail.com>
2021-02-26 17:12:26 -0600

    Removed responses

--- pages/api/v1/authorization/logIn/index.ts
+++ pages/api/v1/authorization/logIn/index.ts
@@ -7,24 +7,19 @@ import JsonWebToken from 'jsonwebtoken'
 const index = async (request: NextApiRequest, response: NextApiResponse) => {
   const { database, client } = await connect()
   const { email, password } = request.body
-  response.status(200).json({ good: 'here 1' })
 
   const account = await database.collection('dotts_accounts').findOne({
     email: email,
   })
-  response.status(200).json({ good: 'here 2' })
 
   if (!account) {
     response.status(200).json({ error: 'No user exists with that email.' })
     return
   }
-  response.status(200).json({ good: 'here 3' })
 
   client.close()
-  response.status(200).json({ good: 'here 4' })
 
   const hashedPassword = account.password
-  response.status(200).json({ good: 'here 5' })
 
   bcrypt.compare(password, hashedPassword, async (error, result) => {
     if (error != null) {
