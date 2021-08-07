251952f177482b7faac8c389b39e10c5b3f4eed9
Kolt Ewing <koltewing@Kolts-Air.attlocal.net>
2021-08-07 14:35:44 -0400

    update the trade detail page to look nicer and be more useful

--- pages/Trading/MyTrades/[_id].jsx
+++ pages/Trading/MyTrades/[_id].jsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useQuery } from 'react-query'
 import { useRouter } from 'next/router'
 import { DOTTS_ACCESS_TOKEN } from '../../../utils/constants'
 import {
   Box,
   Button,
+  Card,
+  Chip,
+  Divider,
+  Grid,
+  Typography,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
+  LinearProgress,
 } from '@material-ui/core'
+import { formatDistance, parseISO, subDays } from 'date-fns'
+import { getCurrentUser, getTradeById } from '../../../utils/requestTemplates'
+
+const getChipColor = (tradeStatus) => {
+  switch (tradeStatus) {
+    case 'pending':
+      return 'primary'
+    case 'declined':
+      return 'secondary'
+    default:
+      return 'default'
+  }
+}
 
 const declineTrade = async (tradeId) => {
   await axios({
@@ -69,48 +89,31 @@ const deleteTrade = async (currentTrade) => {
 const TradePage = () => {
   const router = useRouter()
   const { _id } = router.query
-  const [currentTrade, setCurrentTrade] = useState(null)
-  const [currentUser, setCurrentUser] = useState(null)
-  const [offeringUserId, setOfferingUserId] = useState('')
-  const [receivingUserId, setReceivingUserId] = useState('')
   const [dialogOpen, setDialogOpen] = useState(false)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const user = await axios({
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem(DOTTS_ACCESS_TOKEN),
-        },
-        method: 'post',
-        url: `/api/v1/users/currentUser`,
-        data: {},
-      })
+  const {
+    isLoading: currentUserLoading,
+    error: currentUserError,
+    data: currentUserData,
+  } = useQuery('getCurrentUser', async () => {
+    const response = await getCurrentUser()
+    return response
+  })
 
-      if (user.data.error) {
-      }
+  const currentUserVal = !currentUserLoading && currentUserData.data.account._id
 
-      setCurrentUser(user.data.account)
-    }
-
-    fetchData()
-  }, [])
-
-  useEffect(() => {
-    const fetchTrade = async () => {
-      const tradeById = await axios({
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem(DOTTS_ACCESS_TOKEN),
-        },
-        method: 'post',
-        url: `/api/v1/trades/tradeById`,
-        data: { _id: _id },
-      })
-
-      setCurrentTrade(tradeById.data[0])
-    }
-
-    fetchTrade()
-  }, [])
+  const {
+    isLoading: tradeByIdLoading,
+    error: tradeByIdError,
+    data: tradeByIdData,
+  } = useQuery(
+    ['tradeById'],
+    async () => {
+      const response = await getTradeById(_id)
+      return response
+    },
+    { enabled: !!currentUserVal }
+  )
 
   const handleOpen = () => {
     setDialogOpen(true)
@@ -147,80 +150,175 @@ const TradePage = () => {
     )
   }
 
-  return (
-    <Box mt={2}>
-      <div>Trade Page for {_id}</div>
-      <div>Trade Status: {currentTrade && currentTrade.tradeStatus} </div>
-      <div>
-        OfferingUser:{' '}
-        {currentTrade && currentTrade.offeringUserInfo[0].isflUsername}
-      </div>
-      {currentUser &&
-        currentTrade &&
-        currentUser._id === currentTrade.offeringUserId && (
-          <>
-            <Button size="small" color="secondary" onClick={() => handleOpen()}>
-              Delete Trade
-            </Button>
-            <Dialog
-              open={dialogOpen}
-              onClose={handleClose}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title">
-                {'Delete Trade?'}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                  Are you sure you would like to delete this trade?
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={() => setDialogOpen(false)} color="secondary">
-                  Cancel
-                </Button>
+  if (tradeByIdLoading) return <LinearProgress />
+
+  if (currentUserError || tradeByIdError) return `An error has occurred`
+
+  const currentTrade =
+    tradeByIdData && tradeByIdData.data && tradeByIdData.data[0]
+
+  const isUserTrade =
+    tradeByIdData &&
+    tradeByIdData.data &&
+    tradeByIdData.data[0].offeringUserId === currentUserData.data.account._id
+
+  if (currentTrade) {
+    const {
+      offeringUserInfo,
+      offeringUserCardIds,
+      receivingUserCardIds,
+      receivingUserInfo,
+      offeringUserId,
+      receivingUserId,
+      tradeOfferDate,
+    } = currentTrade
+
+    return (
+      <Box mb={2}>
+        <Card>
+          <Box p={2} mb={2}>
+            <Grid container alignItems="flex-start" direction="column">
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Typography variant="h4" component="h2">
+                  {isUserTrade ? 'Outgoing' : 'Incoming'}
+                </Typography>
+                <Chip
+                  label={currentTrade.tradeStatus}
+                  color={getChipColor(currentTrade.tradeStatus)}
+                />
+              </Grid>
+              <Grid item>
+                <Typography
+                  color="textSecondary"
+                  variant="subtitle2"
+                  component="h6"
+                >
+                  Trade with{' '}
+                  {isUserTrade
+                    ? receivingUserInfo[0].isflUsername
+                    : offeringUserInfo[0].isflUsername}{' '}
+                  -{' '}
+                  {formatDistance(
+                    subDays(parseISO(tradeOfferDate), 0),
+                    new Date(),
+                    {
+                      addSuffix: true,
+                    }
+                  )}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+          <Divider />
+          <Box p={2} mt={2}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <Typography variant="h5" component="h3">
+                  {isUserTrade ? 'Your' : 'Their'} Offer:{' '}
+                </Typography>
+                {offeringUserCardIds.map((card, index) => (
+                  <div key={`${offeringUserId}-${tradeOfferDate}-${index}`}>
+                    {card.playerName} ({card.rarity}){' '}
+                  </div>
+                ))}
+              </Grid>
+              <Grid item>
+                <Typography variant="h5" component="h3">
+                  {isUserTrade ? 'Your' : 'Their'} Request:{' '}
+                </Typography>
+                {receivingUserCardIds.map((card, index) => (
+                  <div key={`${receivingUserId}-${tradeOfferDate}-${index}`}>
+                    {card.playerName} ({card.rarity})
+                  </div>
+                ))}
+              </Grid>
+            </Grid>
+          </Box>
+        </Card>
+        <Box mt={2}>
+          {currentUserData &&
+            currentUserData.data.account &&
+            currentTrade &&
+            currentUserData.data.account._id === offeringUserId && (
+              <>
                 <Button
-                  onClick={() => handleDeleteTrade(currentTrade._id)}
-                  color="primary"
-                  autoFocus
+                  size="small"
+                  color="secondary"
+                  onClick={() => handleOpen()}
                 >
                   Delete Trade
                 </Button>
-              </DialogActions>
-            </Dialog>
-          </>
-        )}
-      {currentUser &&
-        currentTrade &&
-        currentUser._id !== currentTrade.offeringUserId &&
-        currentTrade.tradeStatus === 'pending' && (
-          <>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() =>
-                handleAcceptTrade(
-                  currentTrade.offeringUserId,
-                  currentTrade.offeringUserCardIds.map((card) => card._id),
-                  currentTrade.receivingUserId,
-                  currentTrade.receivingUserCardIds.map((card) => card._id)
-                )
-              }
-            >
-              Accept
-            </Button>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() => handleDeclineTrade(currentTrade._id)}
-            >
-              Decline
-            </Button>
-          </>
-        )}
-    </Box>
-  )
+                <Dialog
+                  open={dialogOpen}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogTitle id="alert-dialog-title">
+                    {'Delete Trade?'}
+                  </DialogTitle>
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      Are you sure you would like to delete this trade?
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button
+                      onClick={() => setDialogOpen(false)}
+                      color="secondary"
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      onClick={() => handleDeleteTrade(currentTrade._id)}
+                      color="primary"
+                      autoFocus
+                    >
+                      Delete Trade
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </>
+            )}
+          {currentUserData &&
+            currentUserData.data.account &&
+            currentTrade &&
+            currentUserData.data.account._id !== offeringUserId &&
+            currentTrade.tradeStatus === 'pending' && (
+              <>
+                <Button
+                  size="small"
+                  color="secondary"
+                  onClick={() =>
+                    handleAcceptTrade(
+                      offeringUserId,
+                      offeringUserCardIds.map((card) => card._id),
+                      receivingUserId,
+                      receivingUserCardIds.map((card) => card._id)
+                    )
+                  }
+                >
+                  Accept
+                </Button>
+                <Button
+                  size="small"
+                  color="secondary"
+                  onClick={() => handleDeclineTrade(currentTrade._id)}
+                >
+                  Decline
+                </Button>
+              </>
+            )}
+        </Box>
+      </Box>
+    )
+  }
+  return null
 }
 
 export default TradePage
