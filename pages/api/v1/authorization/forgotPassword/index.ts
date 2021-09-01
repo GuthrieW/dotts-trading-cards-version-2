677d57a4469e80597bf6c53a982639358f671428
GuthrieW <wesleyeguthrie@gmail.com>
2021-09-01 11:59:22 -0400

    Switching email service

--- pages/api/v1/authorization/forgotPassword/index.ts
+++ pages/api/v1/authorization/forgotPassword/index.ts
@@ -36,7 +36,7 @@ const index = async (request: NextApiRequest, response: NextApiResponse) => {
   client.close()
 
   const transporter = NodeMailer.createTransport({
-    service: 'gmail',
+    service: 'SendPulse',
     auth: {
       user: process.env.DOTTS_EMAIL_USER,
       pass: process.env.DOTTS_EMAIL_PASSWORD,
