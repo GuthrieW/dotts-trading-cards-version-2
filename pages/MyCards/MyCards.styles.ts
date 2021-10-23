0a0d74f473c8adc48c765f898c351260f55fdf66
koltclassic <koltewing@gmail.com>
2021-10-23 11:25:47 -0400

    add trophy room

--- pages/MyCards/MyCards.styles.ts
+++ pages/MyCards/MyCards.styles.ts
@@ -5,6 +5,17 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flex: '1 1 auto',
     flexDirection: 'column',
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  cardDisplay: {
+    height: 150,
+    paddingLeft: 0,
+    [theme.breakpoints.up('md')]: {
+      marginLeft: 32,
+      height: 200,
+      textAlign: 'center',
+    },
   },
 }))
 
