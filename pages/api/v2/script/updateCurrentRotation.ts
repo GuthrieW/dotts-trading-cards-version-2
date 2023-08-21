8e3751c412eed2e2177dd293d101cb59f944e4e6
Wesley Guthrie <wesleyeguthrie@gmail.com>
2023-08-20 20:18:28 -0500

    change method of fixing currentRotation

--- pages/api/v2/script/updateCurrentRotation.ts
+++ pages/api/v2/script/updateCurrentRotation.ts
@@ -3,13 +3,23 @@ import { TableNames } from '../common'
 import { BASE } from '../../../../utils/constants'
 
 export const updateCurrentRotation = async (database: Db): Promise<void> => {
-  await database
-    .collection(TableNames.DOTTS_CARDS)
-    .updateMany({ currentRotation: true }, { $set: { currentRotation: false } })
+  // await database
+  //   .collection(TableNames.DOTTS_CARDS)
+  //   .updateMany({ currentRotation: true }, { $set: { currentRotation: false } })
+
+  // await database
+  //   .collection(TableNames.DOTTS_CARDS)
+  //   .updateMany({ rarity: BASE }, { $set: { currentRotation: true } })
+
+  const afterDate = new Date()
+  afterDate.setMonth(afterDate.getMonth() - 1)
 
   await database
     .collection(TableNames.DOTTS_CARDS)
-    .updateMany({ rarity: BASE }, { $set: { currentRotation: true } })
+    .updateMany(
+      { submissionDate: { $gt: afterDate.toISOString() } },
+      { $set: { currentRotation: true } }
+    )
 
   return
 }
