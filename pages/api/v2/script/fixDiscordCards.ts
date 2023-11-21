fbcdb9b00dfe5b5c4853b09bbb91f95c0bc20182
Wesley Guthrie <wesleyeguthrie@gmail.com>
2023-11-21 14:48:19 -0500

    fix attribute effected

--- pages/api/v2/script/fixDiscordCards.ts
+++ pages/api/v2/script/fixDiscordCards.ts
@@ -6,23 +6,29 @@ export const fixDiscordCards = async (
   scriptData: string
 ): Promise<any[]> => {
   const cardsData = JSON.parse(scriptData)
-  return Object.values(cardsData).map(async (cardData) => {
-    const { _id, new_image_url, old_image_url } = cardData as {
-      _id
-      new_image_url
-      old_image_url
-    }
+  const result = await Promise.all(
+    await Object.values(cardsData).map(async (cardData) => {
+      const { _id, new_image_url, old_image_url } = cardData as {
+        _id
+        new_image_url
+        old_image_url
+      }
 
-    const card = await database
-      .collection(TableNames.DOTTS_CARDS)
-      .findOneAndUpdate(
-        { _id: new ObjectId(_id) },
-        { $set: { image_url: new_image_url } }
-      )
+      console.log('cardData', cardData)
 
-    return {
-      ...card,
-      imageUrl: new_image_url,
-    }
-  })
+      await database
+        .collection(TableNames.DOTTS_CARDS)
+        .findOneAndUpdate(
+          { _id: new ObjectId(_id) },
+          { $set: { imageUrl: new_image_url } }
+        )
+
+      return {
+        _id,
+        imageUrl: new_image_url,
+      }
+    })
+  )
+
+  return result
 }
