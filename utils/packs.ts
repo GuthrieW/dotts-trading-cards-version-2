53fe8590dc01e5a4dc4f882b48eedc7d6b434d39
Wesley Guthrie <wesleyeguthrie@gmail.com>
2024-02-28 10:18:30 -0600

    fixed pack covers

--- utils/packs.ts
+++ utils/packs.ts
@@ -15,14 +15,12 @@ export const PACK_TYPES = [
   {
     type: 'regular',
     name: 'Regular Pack',
-    imageUrl:
-      'https://cdn.discordapp.com/attachments/1128513370048438333/1144126080136007761/contest_pack.png',
+    imageUrl: 'https://i.postimg.cc/BvtTzhyB/contest-pack.png',
   },
   {
     type: 'ultimus',
     name: 'Ultimus Pack',
-    imageUrl:
-      'https://cdn.discordapp.com/attachments/719410556578299954/930916112604995685/Pack_Template.png',
+    imageUrl: 'https://i.postimg.cc/XYsKQM2v/Pack-Template.png',
   },
 ]
 
