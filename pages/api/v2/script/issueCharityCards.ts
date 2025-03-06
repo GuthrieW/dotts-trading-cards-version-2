36e61fa1b5f237bbb486f3a5389a60beb889b3b1
Wesley Guthrie <wesleyeguthrie@gmail.com>
2025-03-05 22:08:04 -0600

    update issue charity cards script

--- pages/api/v2/script/issueCharityCards.ts
+++ pages/api/v2/script/issueCharityCards.ts
@@ -4,94 +4,47 @@ import { TableNames } from '../common'
 export const issueCharityCards = async (database: Db): Promise<void> => {
   const usersToAddCards: { name: string; cards: string[] }[] = [
     {
-      name: 'OrbitingDeath',
+      name: 'Raven',
+      cards: ['653dcd2f95a875e6a766d398'],
+    },
+    {
+      name: 'swoosh',
+      cards: ['653dba0a51984f2ffc582ee2', '653dcd0395a875e6a766d397'],
+    },
+    {
+      name: 'Tmoney6996',
+      cards: ['653b400c499b4ef4f291a7fe'],
+    },
+    {
+      name: 'DarknessRising',
+      cards: ['67c7c6b869c7773a9e1c2a75'],
+    },
+    {
+      name: 'Laser',
+      cards: ['67c7e8b87611e45b2345ddf0'],
+    },
+    {
+      name: 'LtHudz',
+      cards: ['67c7dd914527b0aeff76cb14'],
+    },
+    {
+      name: 'juniped',
       cards: [
-        // '635bf65d2f71667765148d7e', // Nova Montagne - Captain - NOT CREATED
-        // '64e37c0f8f52d15695cc1984', // Stan Francisco - Franchise Icon
-        // '64e37d9c8f52d15695cc1987', // Immanuel Blackstone Captain
-        '5f754423f26c5400176aab87', // Asher Montain Rookie
+        '67c7dd804527b0aeff76cb13',
+        '67c7dd714527b0aeff76cb12',
+        '67c7dd5e4527b0aeff76cb11',
+        '67c7eebe57e2d5533e591de5',
       ],
     },
-    // {
-    //   name: 'ConnorM123',
-    //   cards: [
-    //     '64e37c918f52d15695cc1985', // Franklin Armstrong Franchise Icon
-    //   ],
-    // },
-    // {
-    //   name: 'Kotasa',
-    //   cards: [
-    //     '64e37cd1f6541782e5f038a8', // Nate Swift Franchise Icon
-    //     // '', // Dermot Lavelle Sr. DPOTY Award
-    //   ],
-    // },
-    // {
-    //   name: 'qWest',
-    //   cards: [
-    //     '612cf2324ec24f0008a40fca', // S22 Rookie Auto Tatsu Nakamura, RB #7, OCO, Taylor Rapp Render
-    //     '61cca4a3122fdf0008d1d31e', // S21 Awards Winner MVP Franklin Armstrong, QB #1, OCO
-    //     '61db018c4370c7000918a8e2', // S22 Ultimus Champion Mo Berry, LB #7, COL, Mohamed Barry Render
-    //     '60e9c1f7b92e9b00098d09df', // S18 Rookie Auto Jay Cue, QB #10, AZ, Johnny Manziel Render
-    //   ],
-    // },
-    // {
-    //   name: 'shadyshoelace',
-    //   cards: [
-    //     '61ce1355fec58e000826600a', // Rookie card - Nero Alexander - Wraiths - DE - #92 - Chris Long
-    //     '61cca002d256dc0008c77716', // Award card - S17 Defensive Lineman of the Year - Nero Alexander - Wraiths - #92 - Chris Long
-    //   ],
-    // },
-    // CARD NOT MADE YET
-    // {
-    //   name: 'Laser',
-    //   cards: [
-    //     '', // Dexter Zaylren - Autograph Rookie - Not created
-    //     '612cf1bf4ec24f0008a40fc8', // (Korrin Abernathy Rookie Series, render Ross Cockrell, #31)
-    //   ],
-    // },
-    // CARD NOT MADE YET
-    // {
-    //   name: 'LimJahey',
-    //   cards: [
-    //     '', // Von Hayes - Autograph Rookie - Not created
-    //   ],
-    // },
-    // CARD NOT MADE YET
-    // {
-    //   name: 'Fleshbagsoup',
-    //   cards: [
-    //     '', // A unique card he has not specified yet
-    //   ],
-    // },
-    // USER DOES NOT EXIST
-    // {
-    //   name: 'Starboy',
-    //   cards: [
-    //     '64e37f5e8f52d15695cc1988', // James Bishop - Ultimus Champion card
-    //   ],
-    // },
-    // {
-    //   name: 'DarknessRising',
-    //   cards: [
-    //     '5f754423f26c5400176aab87', // Asher Montain - Autograph Rookie - Not created
-    //   ],
-    // },
-    // {
-    //   name: 'JuOSu',
-    //   cards: [
-    //     '64e37c0f8f52d15695cc1984', // Stan Francisco - Franchise Icon 1/3 - Created but not approved
-    //   ],
-    // },
-    // {
-    //   name: 'PMoney',
-    //   cards: [
-    //     '64e37c0f8f52d15695cc1984', // Stan Francisco - Franchise Icon 3/3 - Not created
-    //   ],
-    // },
-    // {
-    //   name: 'br0_0ker',
-    //   cards: ['64e37e3ff6541782e5f038a9'],
-    // },
+    {
+      name: 'Mooty99',
+      cards: [
+        '5eefe539556d00001779ff9f',
+        '60bec8ac444bc70009772c17',
+        '65adeb0d07eb954542b33a51',
+        '615a75e1e83ae90008757a02',
+      ],
+    },
   ]
 
   await Promise.all(
