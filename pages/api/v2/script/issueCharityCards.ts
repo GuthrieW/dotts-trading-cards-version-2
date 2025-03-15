eebef0263244f13a4b759be7198bc814d78ea617
Wesley Guthrie <wesleyeguthrie@gmail.com>
2025-03-15 15:53:56 -0500

    raven charity cards

--- pages/api/v2/script/issueCharityCards.ts
+++ pages/api/v2/script/issueCharityCards.ts
@@ -5,45 +5,7 @@ export const issueCharityCards = async (database: Db): Promise<void> => {
   const usersToAddCards: { name: string; cards: string[] }[] = [
     {
       name: 'Raven',
-      cards: ['653dcd2f95a875e6a766d398'],
-    },
-    {
-      name: 'swoosh',
-      cards: ['653dba0a51984f2ffc582ee2', '653dcd0395a875e6a766d397'],
-    },
-    {
-      name: 'Tmoney6996',
-      cards: ['653b400c499b4ef4f291a7fe'],
-    },
-    {
-      name: 'DarknessRising',
-      cards: ['67c7c6b869c7773a9e1c2a75'],
-    },
-    {
-      name: 'Laser',
-      cards: ['67c7e8b87611e45b2345ddf0'],
-    },
-    {
-      name: 'LtHudz',
-      cards: ['67c7dd914527b0aeff76cb14'],
-    },
-    {
-      name: 'juniped',
-      cards: [
-        '67c7dd804527b0aeff76cb13',
-        '67c7dd714527b0aeff76cb12',
-        '67c7dd5e4527b0aeff76cb11',
-        '67c7eebe57e2d5533e591de5',
-      ],
-    },
-    {
-      name: 'Mooty99',
-      cards: [
-        '5eefe539556d00001779ff9f',
-        '60bec8ac444bc70009772c17',
-        '65adeb0d07eb954542b33a51',
-        '615a75e1e83ae90008757a02',
-      ],
+      cards: ['64e6c5de3f2686b64a814558', '60e6001af3d462000823c542'],
     },
   ]
 
