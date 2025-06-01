fdfbd1adf476894869213dc02a80168843401a45
Wesley Guthrie <wesleyeguthrie@gmail.com>
2025-06-01 15:52:40 -0500

    update issue cards script

--- pages/api/v2/script/issueCharityCards.ts
+++ pages/api/v2/script/issueCharityCards.ts
@@ -1,11 +1,68 @@
 import { Db } from 'mongodb'
 import { TableNames } from '../common'
 
+// 6831115c879c79c39a374cdc - Zag
+// 68311150879c79c39a374cdb - ValorX77
+// 68311148879c79c39a374cda - Raven
+// 68311141879c79c39a374cd9 - OrbitingDeath
+// 68311139879c79c39a374cd8 - lemonoppy
+// 68311132879c79c39a374cd7 - Kyamprac
+// 68311128879c79c39a374cd6 - jeffie43
+// 68311120879c79c39a374cd5 - JDC4654
+// 683111112ac87c8bb803dc74 - Im2Klutch
+// 683111082ac87c8bb803dc73 - enigmatic
+// 683110fe06848f7209e6c45a - caltroit_red_flames
+// 683110f506848f7209e6c459 - Amidships
+
 export const issueCharityCards = async (database: Db): Promise<void> => {
   const usersToAddCards: { name: string; cards: string[] }[] = [
     {
+      name: 'Zag',
+      cards: ['6831115c879c79c39a374cdc'],
+    },
+    {
+      name: 'ValorX77',
+      cards: ['68311150879c79c39a374cdb'],
+    },
+    {
       name: 'Raven',
-      cards: ['64e6c5de3f2686b64a814558', '60e6001af3d462000823c542'],
+      cards: ['68311148879c79c39a374cda'],
+    },
+    {
+      name: 'OrbitingDeath',
+      cards: ['68311141879c79c39a374cd9'],
+    },
+    {
+      name: 'lemonoppy',
+      cards: ['68311139879c79c39a374cd8'],
+    },
+    {
+      name: 'Kyamprac',
+      cards: ['68311132879c79c39a374cd7'],
+    },
+    {
+      name: 'jeffie43',
+      cards: ['68311128879c79c39a374cd6'],
+    },
+    {
+      name: 'JDC4654',
+      cards: ['68311120879c79c39a374cd5'],
+    },
+    {
+      name: 'Im2Klutch',
+      cards: ['683111112ac87c8bb803dc74'],
+    },
+    {
+      name: 'enigmatic',
+      cards: ['683111082ac87c8bb803dc73'],
+    },
+    {
+      name: 'caltroit_red_flames',
+      cards: ['683110fe06848f7209e6c45a'],
+    },
+    {
+      name: 'Amidships',
+      cards: ['683110f506848f7209e6c459'],
     },
   ]
 
