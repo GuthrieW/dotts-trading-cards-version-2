26ad9bd6f438d5fa813fe9fe4d7684c5e20af8a2
Wesley Guthrie <wesleyeguthrie@gmail.com>
2025-06-14 08:58:59 -0500

    fix name

--- pages/api/v2/script/issueCharityCards.ts
+++ pages/api/v2/script/issueCharityCards.ts
@@ -17,52 +17,8 @@ import { TableNames } from '../common'
 export const issueCharityCards = async (database: Db): Promise<void> => {
   const usersToAddCards: { name: string; cards: string[] }[] = [
     {
-      name: 'Zag',
-      cards: ['6831115c879c79c39a374cdc'],
-    },
-    {
-      name: 'ValorX77',
-      cards: ['68311150879c79c39a374cdb'],
-    },
-    {
-      name: 'Raven',
-      cards: ['68311148879c79c39a374cda'],
-    },
-    {
-      name: 'OrbitingDeath',
-      cards: ['68311141879c79c39a374cd9'],
-    },
-    {
-      name: 'lemonoppy',
-      cards: ['68311139879c79c39a374cd8'],
-    },
-    {
-      name: 'Kyamprac',
-      cards: ['68311132879c79c39a374cd7'],
-    },
-    {
-      name: 'jeffie43',
-      cards: ['68311128879c79c39a374cd6'],
-    },
-    {
-      name: 'JDC4654',
-      cards: ['68311120879c79c39a374cd5'],
-    },
-    {
-      name: 'Im2Klutch',
+      name: 'im2klutch',
       cards: ['683111112ac87c8bb803dc74'],
-    },
-    {
-      name: 'enigmatic',
-      cards: ['683111082ac87c8bb803dc73'],
-    },
-    {
-      name: 'caltroit_red_flames',
-      cards: ['683110fe06848f7209e6c45a'],
-    },
-    {
-      name: 'Amidships',
-      cards: ['683110f506848f7209e6c459'],
     },
   ]
 
